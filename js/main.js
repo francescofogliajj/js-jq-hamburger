5c65de5f111cd063b20c7b84fd726dc2e0f5578d
Francesco <francescofogliajj@gmail.com>
2020-11-23 16:21:15 +0100

    Init repository

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("i.fas.fa-bars").click(
+  function() {
+    $(".hamburger-menu").show("slow");
+  }
+);
+
+$("i.fas.fa-times").click(
+  function() {
+    $(".hamburger-menu").hide("slow");
+  }
+);
